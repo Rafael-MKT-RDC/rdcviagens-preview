fe33d57aa73bf09ef81fe9260dcca0709f92b682
Rafael-MKT-RDC <rafael.valenca@rdcviagens.com.br>
2026-06-17 10:36:02 -0300

    feat(cms): studio structure - Pagina Home

--- sanity.config.ts
+++ sanity.config.ts
@@ -23,6 +23,15 @@ export default defineConfig({
                   .documentId('configuracoesGlobais')
                   .title('Configurações Globais'),
               ),
+            S.listItem()
+              .title('Página Home')
+              .id('paginaHome')
+              .child(
+                S.document()
+                  .schemaType('paginaHome')
+                  .documentId('paginaHome')
+                  .title('Página Home'),
+              ),
             S.divider(),
             S.listItem().title('Posts do Blog').schemaType('blogPost').child(S.documentTypeList('blogPost')),
             S.listItem().title('FAQ – Perguntas Frequentes').schemaType('faqCategoria').child(S.documentTypeList('faqCategoria')),
