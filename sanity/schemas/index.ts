6f302559554862175009f11a71a71459e9c5e980
Rafael-MKT-RDC <rafael.valenca@rdcviagens.com.br>
2026-06-17 12:16:23 -0300

    feat(cms): schema paginaSobre + index

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -5,10 +5,12 @@ import parceiroClube from './parceiroClube'
 import destino from './destino'
 import configuracoesGlobais from './configuracoesGlobais'
 import paginaHome from './paginaHome'
+import paginaSobre from './paginaSobre'
 
 export const schemaTypes = [
   configuracoesGlobais,
   paginaHome,
+  paginaSobre,
   blogPost,
   faqCategoria,
   depoimento,
